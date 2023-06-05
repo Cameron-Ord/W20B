6de10739fc9efc48d1eefb6ce8452a0b2efb664f
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-05 16:33:00 -0600

    working

--- python/app.py
+++ python/app.py
@@ -5,10 +5,51 @@ from flask import Flask
 
 app = Flask(__name__)
 
+conn = mariadb.connect(**dbcreds.conn_params)
+cursor = conn.cursor
 
+def end_conn():
+    if(cursor !=None):
+        cursor.close()
+    if(conn !=None):
+        conn.close()
 
+app.get('/dogs')
+def select_dogs():
+    cursor.execute('CALL select_dogs()')
+    results = cursor.fetchall()
 
+    if(type(results) == list):
+        json = json.dumps(results, default=str)
+        end_conn()
+        return json
+    else:
+        print("error")
+        end_conn()
 
+app.get('/cats')
+def select_cats():
+    cursor.execute('CALL select_cats()')
+    results = cursor.fetchall()
+    if(type(results) == list):
+        json = json.dumps(results, default=str)
+        end_conn()
+        return json
+    else:
+        print("error")
+        end_conn()
+
+app.get('/animals')
+def select_animals():
+    cursor.execute('CALL select_animals()')
+    results = cursor.fetchall()
+    if(type(results) == list):
+        json = json.dumps(results, default=str)
+        end_conn()
+        return json
+    else:
+        print("error")
+        end_conn()
 
 
 
