b447fa6dbe6eb898c046b814cf6cbcdfcbcc7ef4
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-06 17:55:17 -0600

    added more detailed comments

--- python/app.py
+++ python/app.py
@@ -1,3 +1,4 @@
+#importing
 import json
 from flask import Flask
 import dbhelper
@@ -6,11 +7,13 @@ app = Flask(__name__)
 
 @app.get('/dogs')
 def select_dogs():
+   #function that returns the results from run_proceedure when using respective arguments as JSON
    results = dbhelper.run_proceedure('CALL select_dogs', [])
    results_json = json.dumps(results, default=str)
    return results_json
 
 @app.get('/cats')
+#function that returns the results from run_proceedure when using respective arguments as JSON
 def select_cats():
    results = dbhelper.run_proceedure('CALL select_cats', [])
    results_json = json.dumps(results, default=str)
@@ -18,9 +21,11 @@ def select_cats():
 
 @app.get('/animals')
 def get_animals():
+   #function that returns the results from run_proceedure when using respective arguments as JSON
    results = dbhelper.run_proceedure('CALL select_animals', [])
    results_json = json.dumps(results, default=str)
    return results_json
 
 
+#running @app
 app.run(debug=True)
