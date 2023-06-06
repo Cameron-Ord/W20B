3dab4da82c5892032c47e07211fbb8c223fe63f6
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-06 17:27:35 -0600

    added dbhelpers

--- python/app.py
+++ python/app.py
@@ -2,61 +2,31 @@ import mariadb
 import dbcreds
 import json
 from flask import Flask
+import dbhelper
 
 app = Flask(__name__)
-#establishing connection
+
 
 conn = mariadb.connect(**dbcreds.conn_params)
 cursor = conn.cursor()
 
-def end_conn():
-    if(cursor !=None):
-        cursor.close()
-    if(conn !=None):
-        conn.close()
-
-#closes conn and cursor
-
 @app.get('/dogs')
 def select_dogs():
-    cursor.execute('CALL select_dogs()')
-    results = cursor.fetchall()
-#json stringifies results
-    if(type(results) == list):
-        rst_json = json.dumps(results, default=str)
-        end_conn()
-        return rst_json
-    else:
-        print("error")
-        end_conn()
+   results = dbhelper.run_proceedure('CALL select_dogs', [])
+   results_json = json.dumps(results, default=str)
+   return results_json
 
 @app.get('/cats')
 def select_cats():
-    #calls the proceedure
-    cursor.execute('CALL select_cats()')
-    results = cursor.fetchall()
-#json stringifies results
-    if(type(results) == list):
-        rst_json = json.dumps(results, default=str)
-        end_conn()
-        return rst_json
-    else:
-        print("error")
-        end_conn()
+   results = dbhelper.run_proceedure('CALL select_cats', [])
+   results_json = json.dumps(results, default=str)
+   return results_json
 
 @app.get('/animals')
 def get_animals():
-    #calls the proceedure
-    cursor.execute('CALL select_animals()')
-    results = cursor.fetchall()
-#json stringifies results
-    if(type(results) == list):
-        rst_json = json.dumps(results, default=str)
-        end_conn()
-        return rst_json
-    else:
-        print("error")
-        end_conn()
+   results = dbhelper.run_proceedure('CALL select_animals', [])
+   results_json = json.dumps(results, default=str)
+   return results_json
 
 
 app.run(debug=True)
