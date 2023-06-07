513a4d0a3e80c1aedb5035355bbd6d9991d682b1
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-06 18:02:56 -0600

    error handling

--- python/app.py
+++ python/app.py
@@ -9,22 +9,32 @@ app = Flask(__name__)
 def select_dogs():
    #function that returns the results from run_proceedure when using respective arguments as JSON
    results = dbhelper.run_proceedure('CALL select_dogs', [])
-   results_json = json.dumps(results, default=str)
+   if(type(results)==list):
+    results_json = json.dumps(results, default=str)
+   else:
+      print("error")
    return results_json
 
 @app.get('/cats')
 #function that returns the results from run_proceedure when using respective arguments as JSON
 def select_cats():
    results = dbhelper.run_proceedure('CALL select_cats', [])
-   results_json = json.dumps(results, default=str)
+   if(type(results)==list):
+    results_json = json.dumps(results, default=str)
+   else:
+     print("error")
    return results_json
 
 @app.get('/animals')
 def get_animals():
    #function that returns the results from run_proceedure when using respective arguments as JSON
    results = dbhelper.run_proceedure('CALL select_animals', [])
-   results_json = json.dumps(results, default=str)
+   if(type(results)==list):
+    results_json = json.dumps(results, default=str)
+   else:
+     print("error")
    return results_json
+
 
 
 #running @app
